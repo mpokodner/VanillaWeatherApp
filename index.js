b01c5a52fdf2d232f1f9a2530ee625b6bbc97c91
mpo.coder <mpokodner@gmail.com>
2025-04-07 10:01:42 -0500

    input weather data java

--- index.js
+++ index.js
@@ -1,26 +1,58 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector(".city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#day-time");
+  let date = new Date(response.data.time * 1000);
+
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  temperatureElement.innerHTML = Math.round(temperature);
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
+  console.log(response.data);
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function searchCity(city) {
+  //make api call and update interface
+  let apiKey = "207446fe5b843td6o246060ad31759ff";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector(".search-form-input");
 
   searchCity(searchInput.value);
 }
-
 let searchFormElement = document.querySelector(".search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-searchCity("Paris");
-
-function searchCity(city) {
-  let apiKey = "207446fe5b843td6o246060ad31759ff";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${apiKey}`;
-  axios.get(apiUrl).then(refreshWeather);
-}
-
-function refreshWeather(response) {
-  let temperatureElement = document.querySelector(".current-temperature");
-  let temperature = response.data.temperature.current.value;
-  let cityElement = document.querySelector(".city");
-
-  cityElement.innerHTML = response.data.searchedCity;
-  temperatureElement.innerHTML = `${Math.round(temperature)}`;
-}
