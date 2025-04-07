280425cac080e4f7218b9a17ffe9237393835c19
mpo.coder <mpokodner@gmail.com>
2025-04-06 21:36:03 -0500

    still trying to fix javascript

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-function refreshWeather(response) {
-  let temperatureElement = document.querySelector(".current-temperature");
-  let temperature = response.data.temperature;
-  let cityElement = document.querySelector(".city");
-
-  cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = `${Math.round(temperature)}`;
-}
-function searchCity(city) {
-  let apiKey = "207446fe5b843td6o246060ad31759ff";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  axios.get(apiUrl).then(refreshWeather);
-}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector(".search-form-input");
@@ -22,3 +9,18 @@ let searchFormElement = document.querySelector(".search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+
+function searchCity(city) {
+  let apiKey = "207446fe5b843td6o246060ad31759ff";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector(".current-temperature");
+  let temperature = response.data.temperature.current.value;
+  let cityElement = document.querySelector(".city");
+
+  cityElement.innerHTML = response.data.searchedCity;
+  temperatureElement.innerHTML = `${Math.round(temperature)}`;
+}
